151b5df4931d46dc068f2079c82be12bdde32c55
eh <coreykemn@gmail.com>
2025-06-09 04:08:40 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
 Config = {}
-Config.WeaponAnimation = "always"  -- Options: "always" or "key"
-Config.Keybind = 244                -- Default keybind is "E" (key code 38), used in "key" mode
+
+Config.ChangeWeaponRunningAnimationKey = "L" -- L is default key, you can change from here -> Settings > Key Bindings > FiveM > Change Weapon Running Animation
+
+Config.WeaponAnimation = "always" -- always, key, none
